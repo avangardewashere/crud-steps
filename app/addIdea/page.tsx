aaef6b4af46bed48200ea63fe368ecd44a9c1f30
avangardewashere <avelpanaligan@gmail.com>
2024-12-11 23:38:28 +0800

    add/remove-funcitonality

--- app/addIdea/page.tsx
+++ app/addIdea/page.tsx
@@ -1,16 +1,39 @@
 "use client";
 import { useState } from "react";
+import { GiReturnArrow } from "react-icons/gi";
 
 export default function addTopic() {
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
 
-  const handleSubmit = () => {
-    console.log(title,details);
+  const handleSubmit = async (e: any) => {
+    console.log(title, details);
 
+    e.preventDefault();
 
-  }
+    if (!title || !details) {
+      alert("Title and descriptoin are required");
+      GiReturnArrow;
+    }
 
+    try {
+      const res = await fetch("http://localhost:3000/api/idea", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          body: JSON.stringify({ title, description: details }),
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Error in creating a new idea");
+      }
+
+      return res.json();
+    } catch (e) {
+      console.log("Error in processing the new idea: ", e);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
@@ -34,7 +57,6 @@ export default function addTopic() {
         className="bg-green-600 font-bold text-white py-3 px-6 w-fit "
         type="submit"
       >
-      
         Submit
       </button>
     </form>
--- components/RemoveBtn.tsx
+++ components/RemoveBtn.tsx
@@ -1,9 +1,27 @@
+import { useRouter } from "next/navigation";
 import { HiOutlineTrash } from "react-icons/hi";
 
-export default function RemoveBtn(){
-    return(
-        <button className='text-rose-600' >
-         <HiOutlineTrash  size={24}/>
-        </button>
-    )
+interface IdeaProps {
+  id: string;
+}
+
+export default async function RemoveBtn(props: IdeaProps) {
+  const Router = useRouter();
+  const removeIdea = async () => {
+    const confirmed = confirm("Are you sure?");
+
+    if (confirmed) {
+      const res = await fetch("http://localhost:3000/api/idea?id=" + props.id, {
+        method: "DELETE",
+      });
+
+      Router.refresh();
+    }
+  };
+
+  return (
+    <button onClick={removeIdea} className="text-rose-600">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
 }
